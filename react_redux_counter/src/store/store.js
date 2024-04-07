0bc31eba5e1163c5696e69e6e6fcdf9161b0804a
mithcodes <singhmithlesh791@gmail.com>
2024-04-07 11:12:46 +0530

    react

--- react_redux_counter/src/store/store.js
+++ react_redux_counter/src/store/store.js
@@ -1,24 +1,5 @@
-// import { createStore } from "redux";
 
-// const counterReducer = (state = { counter: 0 }, action) => {
-//   switch (action.type) {
-//     case "INCREMENT": {
-//       return { counter: state.counter + action.payload };
-//     }
 
-//     case "DECREMENT": {
-//       return { counter: state.counter - action.payload };
-//     }
-
-//     default: {
-//       return state;
-//     }
-//   }
-// };
-
-// const store = createStore(counterReducer);
-
-//export default store;
 
 //------------------------------------------------------
 
